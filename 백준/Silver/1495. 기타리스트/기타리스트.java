ef7e5e1ad7cdc1f044c51974ab18f8997a206c20
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-03-28 11:55:36 +0900

    [Silver I] Title: 기타리스트, Time: 136 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -7,24 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N,S,M;
     static int[] V;
-    static int[] dp;
-    static class Node implements Comparable<Node> {
-        int x,idx;
-
-        public Node(int x, int idx) {
-            this.x = x;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if(this.idx == o.idx){
-            	return o.x - this.x;
-            }
-            return this.idx - o.idx;
-        }
-
-    }
+    static boolean[][] dp;
     
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,43 +17,34 @@ public class Main {
         S = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        V = new int[N];
-        dp = new int[N+1];
+        V = new int[N+1];
+        dp = new boolean[51][1001];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             V[i] = Integer.parseInt(st.nextToken());
         }
-        BFS();
+
+        DP(0,S);
+        int ans =-1;
+        for (int i = 0; i <= M; i++) {
+			if(dp[N][i]) ans = i;
+		}
+        System.out.println(ans);
 	}
 	
-	// 런타임 에러 났던 이유 : return -1로 할경우 정상적이지 못한 결과값으로 인식해 런타임 에러날 수 있음
-	static void BFS(){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        // visted 방문처리 하는 이유 : 중복 제거하기 위해
-        boolean[][] visited = new boolean[1001][51];
-        pq.add(new Node(S,0));
-        visited[S][0] = true;
-
-        while(!pq.isEmpty()){
-            Node n = pq.poll();
-
-            if(n.idx == N){
-                System.out.println(n.x);
-                return;
-            }
-
-            if(n.x + V[n.idx] >= 0 && n.x + V[n.idx] <= M && !visited[n.x + V[n.idx]][n.idx]) {
-                pq.add(new Node(n.x + V[n.idx],n.idx+1));
-                visited[n.x + V[n.idx]][n.idx] = true;
-            }
-
-            if(n.x - V[n.idx] >= 0 && n.x - V[n.idx] <= M && !visited[n.x - V[n.idx]][n.idx]){
-                pq.add(new Node(n.x - V[n.idx],n.idx+1));
-                visited[n.x - V[n.idx]][n.idx] = true;
-            }
-        }
-       System.out.println(-1);
-    }
+	static void DP(int idx,int volum) {
+		if(idx > N) return;
+		if(dp[idx][volum]) return;
+		
+		dp[idx][volum] = true;
+		
+		if(volum-V[idx] >=0) {
+			DP(idx+1,volum-V[idx]);
+		}
+		if(volum+V[idx] <=M) {
+			DP(idx+1,volum+V[idx]);
+		}
+		return;
+	}
 }
