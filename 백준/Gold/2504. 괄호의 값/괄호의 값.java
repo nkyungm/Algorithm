130cb170f3a653678a983766792d94f9d731d6ba
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-03-28 15:23:20 +0900

    [Gold V] Title: 괄호의 값, Time: 128 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -16,37 +16,36 @@ public class Main {
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
 
+			// 괄호 시작
 			if(c == '(') {
-				temp*=2;
+				temp*=2; //2곱하고 스택넣어줌
 				stack.add(c);
 			}
 			else if(c=='[') {
-				temp*=3;
+				temp*=3; //3곱하고 스택넣어줌
 				stack.add(c);
-			}else if(c==')') {
-				if(stack.isEmpty()) {
+			}else {
+				if(stack.isEmpty()) { //스택에 아무값도 없는경우, 잘못된 문자열임
 					result = 0;
 					break;
 				}
 				char out = stack.pop();
-				if(out != '(') { //종료 조건
-					result = 0; break;
+				if(c==')') {
+					if(out != '(') { //종료 조건
+						result = 0; break;
+					}
+					if(s.charAt(i-1) =='(') result+=temp;
+					temp/=2;
+					
+				}else if(c==']') {
+					if(out != '[') { //종료 조건
+						result = 0; break;
+					}
+					if(s.charAt(i-1) =='[') result+=temp;
+					temp/=3;
 				}
-				if(s.charAt(i-1) =='(') result+=temp;
-				temp/=2;
-				
-			}else if(c==']') {
-				if(stack.isEmpty()) {
-					result = 0;
-					break;
-				}
-				char out = stack.pop();
-				if(out != '[') { //종료 조건
-					result = 0; break;
-				}
-				if(s.charAt(i-1) =='[') result+=temp;
-				temp/=3;
 			}
+				
 		}
 		if(!stack.isEmpty()) result = 0;
 		System.out.println(result);
