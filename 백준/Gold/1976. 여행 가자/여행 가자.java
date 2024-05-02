574aacda2069b0354739d3dbe3d6e64f1d4f4d9d
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-05-02 17:00:08 +0900

    [Gold IV] Title: 여행 가자, Time: 144 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,55 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    //플로이드 워샬 알고리즘 활용
+    //Union-Find 알고리즘 활용
     static int N,M;
-    static boolean[][] graph;
+    static int[] parents;
+    static List<Node> arr;
+    static int[] arr2;
+    static class Node{
+        int start,end;
+
+        public Node(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        graph = new boolean[N+1][N+1];
+        parents = new int[N+1];
+        arr = new ArrayList<>();
+        arr2=  new int[M];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                graph[i][j] = (num ==1);
+                if(num == 1) arr.add(new Node(i,j));
             }
-            graph[i][i] = true;
         }
-        floyd();
+
+        makeSet();
+
+        for (Node n : arr){
+            int start = n.start;
+            int end = n.end;
+            union(start,end);
+        }
 
         st = new StringTokenizer(br.readLine());
-        int[] arr = new int[M];
         for (int i = 0; i < M; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < M-1; i++) {
-            if(!graph[arr[i]][arr[i+1]]) {
+            int sRoot= findSet(arr2[i]);
+            int eRoot= findSet(arr2[i+1]);
+            if(sRoot != eRoot) {
                 System.out.println("NO");
                 return;
             }
         }
         System.out.println("YES");
 
+
     }
-    static void floyd(){
-        // k: 경유지
-        for (int k = 1; k <=N ; k++) {
-            for (int i = 1; i <=N ; i++) { //i: 출발지
-                for (int j = 1; j <=N ; j++) { // j: 도착지
-                    if(graph[i][k] && graph[k][j]) graph[i][j] = true;
-                }
-            }
+    static void makeSet(){
+        for (int i = 1; i <=N ; i++) {
+            parents[i] = i;
         }
+    }
+    static int findSet(int a){
+        if(parents[a] == a) return a;
+        return parents[a] = findSet(parents[a]);
+    }
+
+    static boolean union (int a,int b){
+        int aRoot = findSet(a);
+        int bRoot = findSet(b);
+
+        if(aRoot == bRoot) return false;
+        parents[bRoot] = aRoot;
+        return true;
     }
 }
