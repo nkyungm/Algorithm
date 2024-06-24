1f4fc542cd7df32ecb02e00856a5e4b02d54d87c
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-06-24 21:44:49 +0900

    [level 3] Title: 단속카메라, Time: 9.62 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+
+// 알고리즘 : 탐욕법
+/*
+1. 첫번째 원소 기준으로 오름차순 정렬
+2. 두번째 값 저장하고 가장 큰값이랑 비교 갱신
+*/
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        int routesSize = routes.length;
+
+        // 주의) 2차원 정렬시 Comparator따로 사용해야함
+        Arrays.sort(routes,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1,int[] o2){
+                return o1[1]-o2[1];
+            }
+        });
+        
+        int maxValue = routes[0][1];
+        for(int i=1;i<routesSize;i++){
+            if(maxValue >= routes[i][0] && maxValue <= routes[i][1]) continue;
+            answer++;
+            maxValue = routes[i][1];
+        }
+        return answer;
+    }
+}
