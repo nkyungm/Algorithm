c99e46f78edc5f3e3060fb53beb408e239da344a
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-08-29 21:10:57 +0900

    [Gold V] Title: 괄호의 값, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,53 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		 
-		Stack<Character> stack = new Stack<>();
-		
-		int temp = 1;
-		int result = 0;
-		
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			// 괄호 시작
-			if(c == '(') {
-				temp*=2; //2곱하고 스택넣어줌
-				stack.add(c);
-			}
-			else if(c=='[') {
-				temp*=3; //3곱하고 스택넣어줌
-				stack.add(c);
-			}else {
-				if(stack.isEmpty()) { //스택에 아무값도 없는경우, 잘못된 문자열임
-					result = 0;
-					break;
-				}
-				char out = stack.pop();
-				if(c==')') {
-					if(out != '(') { //종료 조건
-						result = 0; break;
-					}
-					if(s.charAt(i-1) =='(') result+=temp;
-					temp/=2;
-					
-				}else if(c==']') {
-					if(out != '[') { //종료 조건
-						result = 0; break;
-					}
-					if(s.charAt(i-1) =='[') result+=temp;
-					temp/=3;
-				}
-			}
-				
-		}
-		if(!stack.isEmpty()) result = 0;
-		System.out.println(result);
-	}
+        String s = br.readLine();
+        Deque<Character> stack = new ArrayDeque<>();
+        int answer = 0;
+        int temp = 1;
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.addLast(c);
+                temp *=2;
+                continue;
+            }
+            if(c=='[') {
+                stack.addLast(c);
+                temp *= 3;
+                continue;
+            }
+            if(stack.isEmpty()){
+                System.out.println(0);
+                return;
+            }
+            if(c==')'){
+                if(stack.pollLast() != '('){
+                    System.out.println(0);
+                    return;
+                }
+                if(s.charAt(i-1) == '(') answer+=temp;
+                temp/=2;
+                continue;
+            }
+            if(c==']'){
+                if(stack.pollLast() != '['){
+                    System.out.println(0);
+                    return;
+                }
+                if(s.charAt(i-1) == '[') answer+=temp;
+                temp/=3;
+            }
+        }
+        if(!stack.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(answer);
+    }
 }
+
