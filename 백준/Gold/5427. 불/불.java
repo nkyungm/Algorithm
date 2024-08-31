99bddf87afcca39832bd0a2aa311b41ed6d2e1a0
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-08-31 20:13:26 +0900

    [Gold IV] Title: 불, Time: 528 ms, Memory: 81284 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,108 +1,98 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int W,H;
-	static char[][] graph;
-	static int[][] fire;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
-	static boolean[][] visited;
-	static Node person;
-	static Queue<Node> queue = new ArrayDeque<>();
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st =null;
-		
-		int T= Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			W = Integer.parseInt(st.nextToken());
-			H = Integer.parseInt(st.nextToken());
-			
-			graph = new char[H][W];
-			fire = new int[H][W];
-			visited = new boolean[H][W];
-			
-			for (int i = 0; i < H; i++) {
-				String s = br.readLine();
-				for (int j = 0; j < W; j++) {
-					graph[i][j] = s.charAt(j);
-					if(graph[i][j] == '*') {
-						queue.add(new Node(i, j, 1));
-						visited[i][j] = true;
-						fire[i][j] = 1;
- 					}else if(graph[i][j] == '@') person = new Node(i,j,0);
-				}
-			}
-			
-			// 불 BFS 탐색
-			BFS_fire();
-//			for (int i = 0; i < H; i++) {
-//				System.out.println(Arrays.toString(fire[i]));
-//			}
-			int ans = BFS_person(person);
-			if(ans == -1) System.out.println("IMPOSSIBLE");
-			else System.out.println(ans);
-		}
-	}
-	// 불 BFS
-	static void BFS_fire() {
-		while(!queue.isEmpty()) {
-			Node n = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int xi = n.x + dx[i];
-				int yi = n.y + dy[i];
-				
-				if(xi <0 || xi >= H || yi <0 || yi >=W ) continue;
-				if(visited[xi][yi] || graph[xi][yi] == '#') continue;
-				
-				visited[xi][yi] = true;
-				fire[xi][yi] = n.sec+1;
-				queue.add(new Node(xi,yi,n.sec+1));
-			}
-		}
-	}
-	// 상근 BFS
-	static int BFS_person(Node p) {
-		visited = new boolean[H][W];
-		queue = new ArrayDeque<>();
-		queue.add(p);
-		visited[p.x][p.y] = true;
-		
-		while(!queue.isEmpty()) {
-			Node n = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int xi = n.x + dx[i];
-				int yi = n.y + dy[i];
-				
-				// 탈출 조건
-				if(xi <0 || xi >= H || yi <0 || yi >=W ) {
-					// System.out.println(xi + " "+ yi);
-					return n.sec+1;
-				}
-				if(visited[xi][yi] || graph[xi][yi] == '#') continue;
-				if(fire[xi][yi] >0 && fire[xi][yi] <= n.sec+2) continue;
+    static int W;
+    static int H;
+    static char[][] graph;
+    static boolean[][] visited;
+    static int[][] fire;
+    static Queue<Node> queue;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int[] dS;
+    static StringBuffer sb;
 
-				visited[xi][yi] = true;
-				queue.add(new Node(xi,yi,n.sec+1));
-			}
-		}
-		return -1;
-	}
-	
-	static class Node{
-		int x,y,sec;
-		public Node(int x, int y, int sec) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.sec = sec;
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        sb = new StringBuffer();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t =0;t<T;t++){
+            st = new StringTokenizer(br.readLine());
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            graph = new char[H][W];
+            visited = new boolean[H][W];
+            fire = new int[H][W];
+            queue = new ArrayDeque<>();
+
+            for(int i=0;i<H;i++){
+                String s = br.readLine();
+                for(int j=0;j<W;j++){
+                    char c = s.charAt(j);
+                    graph[i][j] =c;
+                    switch(c){
+                        case '@':
+                            dS = new int[]{i,j};
+                            break;
+                        case '*':
+                            queue.add(new Node(i,j,1));
+                            visited[i][j] = true;
+                            fire[i][j] = 1;
+                            break;
+                    }
+                }
+            }
+            FireBFS();
+            visited = new boolean[H][W];
+            queue.add(new Node(dS[0],dS[1],1));
+            BFS();
+        }
+        System.out.println(sb);
+    }
+    static void FireBFS(){
+        while(!queue.isEmpty()){
+            Node n = queue.poll();
+            for(int i=0;i<4;i++){
+                int xi = n.x + dx[i];
+                int yi = n.y + dy[i];
+                if(xi<0 || xi>=H||yi<0 || yi>=W) continue;
+                if(graph[xi][yi]=='#' || visited[xi][yi]) continue;
+                queue.add(new Node(xi,yi,n.cnt+1));
+                visited[xi][yi] = true;
+                fire[xi][yi] = n.cnt+1;
+            }
+        }
+    }
+    static void BFS(){
+        while(!queue.isEmpty()){
+            Node n = queue.poll();
+            if(n.x==0 || n.x==H-1 || n.y==0 ||n.y==W-1){
+                sb.append(n.cnt).append("\n");
+                return;
+            }
+            for(int i=0;i<4;i++){
+                int xi = n.x + dx[i];
+                int yi = n.y + dy[i];
+                if(xi<0 || xi>=H||yi<0 || yi>=W) continue;
+                if(graph[xi][yi]!='.' || visited[xi][yi]) continue;
+                if(fire[xi][yi]>0 && fire[xi][yi] <= n.cnt+1) continue;
+                queue.add(new Node(xi,yi,n.cnt+1));
+                visited[xi][yi] = true;
+            }
+        }
+        sb.append("IMPOSSIBLE").append("\n");
+    }
+    static class Node{
+        int x;
+        int y;
+        int cnt;
+        Node(int x,int y,int cnt){
+            this.x = x;
+            this.y =y;
+            this.cnt =cnt;
+        }
+    }
 }
