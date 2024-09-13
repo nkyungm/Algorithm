2e6124eb0ebf93b4459aeee55b71b53df9aeb7a5
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-09-13 10:11:31 +0900

    [Silver III] Title: N과 M (6), Time: 64 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static StringBuffer sb = new StringBuffer();
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        Combination(0,new int[M],0);
+        System.out.println(sb);
+    }
+    static void Combination(int toSelect,int[] selected,int selectIdx){
+        if(toSelect ==M){
+            for(int i=0;i<M;i++){
+                sb.append(selected[i]).append(" ");
+            }sb.append("\n");
+            return;
+        }
+        for(int i=selectIdx;i<N;i++){
+            selected[toSelect] = arr[i];
+            Combination(toSelect+1,selected,i+1);
+        }
+    }
+
+}
