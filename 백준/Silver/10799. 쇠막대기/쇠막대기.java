7c96fa01f74acfbeb9848093e4aa2a5788b7f9ff
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-10-01 02:25:38 +0900

    [Silver II] Title: 쇠막대기, Time: 80 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,29 +1,26 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int answer = 0; // 전체 개수 저장 위함
-        Deque<Character> stack = new ArrayDeque<>();
-
+        int answer = 0;
+        int leftCnt = 0;
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            // 1. ( 인경우 -> 스택에 넣기, Scnt 초기화
-            if(c=='('){
-                stack.add(c);
-                continue;
+            // 1. ( 인 경우 -> +1
+            if(c == '(') leftCnt++;
+            else{
+                // 2. ) 인데 바로 앞에 (인경우 -> leftCnt--하고 answer에 추가
+                if(s.charAt(i-1) == '('){
+                    leftCnt--;
+                    answer += leftCnt;
+                }else{
+                    answer++;
+                    leftCnt--;
+                }
             }
-            // 2. ) 인경우
-            stack.pollLast(); // 미리 꺼내주기
-            if(s.charAt(i-1) == '('){ // 1번 경우
-                answer += stack.size();
-            }else answer++;
-
-            // System.out.println(Scnt+ " "+Lcnt+" "+answer);
         }
-
         System.out.println(answer);
     }
 }
