bf44982452cd69048ccb21fe3d28e53d20110cee
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-10-01 18:49:03 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 20244 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,30 +1,31 @@
 import java.util.*;
 import java.io.*;
+// 순열
+// permutation(int cnt,int[] selected,boolean[] visited)
 public class Main {
-    static int N,M;
-    static StringBuffer sb = new StringBuffer();
-    public static void main(String[] args) throws IOException{
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        Permutation(0,new int[M],new boolean[N]);
+        permutation(0,new int[M],new boolean[N]);
         System.out.println(sb);
     }
-    static void Permutation(int toSelect,int[] selected,boolean[] visited){
-        if(toSelect == M){
+    static void permutation(int cnt,int[] selected,boolean[] visited){
+        if(cnt == M){
             for(int i=0;i<M;i++){
-                sb.append(selected[i]).append(" ");
+                sb.append(selected[i]+1).append(" ");
             }sb.append("\n");
             return;
         }
         for(int i=0;i<N;i++){
             if(!visited[i]){
                 visited[i] = true;
-                selected[toSelect] = i+1;
-                Permutation(toSelect+1,selected,visited);
+                selected[cnt] = i;
+                permutation(cnt+1,selected,visited);
                 visited[i] = false;
             }
         }
