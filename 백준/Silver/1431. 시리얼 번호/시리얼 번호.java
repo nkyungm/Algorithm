0e753805d0ce98e06db60af0f4ca1a9555740acf
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-10-01 19:33:20 +0900

    [Silver III] Title: 시리얼 번호, Time: 188 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            list.add(br.readLine());
+        }
+
+        Collections.sort(list,(o1,o2) ->{
+            if(o1.length() == o2.length()){
+                // 길이 같으면 숫자만 다 더해서 더 작은거
+                int aSum = 0;
+                int bSum = 0;
+                for(int i=0;i<o1.length();i++){
+                    if(o1.charAt(i) >= 'A' && o1.charAt(i) <= 'Z') continue;
+                    aSum += Character.getNumericValue(o1.charAt(i));
+                }
+                for(int i=0;i<o2.length();i++){
+                    if(o2.charAt(i) >= 'A' && o2.charAt(i) <= 'Z') continue;
+                    bSum += Character.getNumericValue(o2.charAt(i));
+                }
+                if(aSum == bSum){
+                    return o1.compareTo(o2);
+                }
+                return aSum - bSum;
+            }
+            return o1.length() - o2.length();
+        }
+        );
+
+        for(int i=0;i<N;i++){
+           System.out.println(list.get(i));
+        }
+    }
+}
