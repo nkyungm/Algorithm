bc765c78c9e4b1dd1b56815905f9a2326942d950
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-03 19:04:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 648 ms, Memory: 127396 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,14 +1,17 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main {
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+    static int[][] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st  =new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr =new int[N+1][N+1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr =new int[N+1][N+1];
 
         for(int i=1;i<=N;i++){
             st = new StringTokenizer(br.readLine());
@@ -23,11 +26,12 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            sb.append(arr[x2][y2]-(arr[x2][y1-1]+arr[x1-1][y2]-arr[x1-1][y1-1])).append("\n");
+            calculate(x1,y1,x2,y2);
         }
-
         System.out.println(sb);
-
-
+    }
+    static void calculate(int x1,int y1,int x2, int y2){
+        int result = arr[x2][y2]-(arr[x2][y1-1]+arr[x1-1][y2]-arr[x1-1][y1-1]);
+        sb.append(result).append("\n");
     }
 }
