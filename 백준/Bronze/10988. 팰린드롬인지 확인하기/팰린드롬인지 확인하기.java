f4be9604c96253c956b7bc68b498ccf2c964175d
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-05 00:39:06 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = s.length();
+
+        for(int i=0;i<n/2;i++) {
+            if (s.charAt(i) == s.charAt(n - i - 1)) continue;
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(1);
+    }
+}
