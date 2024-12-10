790a07fbb30c65ed0775e10d9910f7878e714d48
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-10 16:26:12 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 392 ms, Memory: 54488 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,19 +1,19 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuffer sb = new StringBuffer();
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        HashMap<Integer,String> map = new HashMap<>();
+        String[] arr = new String[N+1];
         HashMap<String,Integer> map2 = new HashMap<>();
 
         for(int i=1;i<=N;i++) {
             String name = br.readLine();
-            map.put(i,name);
+            arr[i] = name;
             map2.put(name,i);
         }
 
@@ -21,7 +21,7 @@ public class Main {
             String s = br.readLine();
             // 숫자일 경우
             if(Character.isDigit(s.charAt(0))){
-                sb.append(map.get(Integer.parseInt(s))).append("\n");
+                sb.append(arr[Integer.parseInt(s)]).append("\n");
             }
             // 문자일 경우
             else sb.append(map2.get(s)).append("\n");
