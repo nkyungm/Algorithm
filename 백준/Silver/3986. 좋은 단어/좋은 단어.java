a1035a7ecc0fb9e9db74e0ad26efb5908c0f2220
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-16 18:37:41 +0900

    [Silver IV] Title: 좋은 단어, Time: 144 ms, Memory: 21832 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,40 +1,33 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int answer = 0;
 
         for(int i=0;i<N;i++){
+            ArrayDeque<Character> stack= new ArrayDeque<>();
             String s = br.readLine();
-            Deque<Character> stack = new ArrayDeque<>();
-            boolean flag = true;
+            // 홀수면 넘기기
+            if(s.length()%2!=0) continue;
             for(char c : s.toCharArray()){
-                if(stack.isEmpty()){
-                    stack.add(c);
-                    continue;
-                }
-                if(c=='A'){
-                    int num = stack.peekLast();
-                    if(num!='A') {
-                        stack.add(c);
-                        continue;
-                    }
-                    stack.pollLast();
-                }else{
-                    int num = stack.peekLast();
-                    if(num!='B') {
-                        stack.add(c);
-                        continue;
-                    }
-                    stack.pollLast();
+                // 스택이 비어있으면 넣기
+                if(stack.isEmpty()) stack.offerLast(c);
+                else{
+                    // 비어있지 않은경우
+                    // 1. 스택 맨위에 있는거 꺼내기
+                    char last = stack.peekLast();
+                    // 2. 같으면 빼고 다르면 넣기
+                    if(last == c) stack.pollLast();
+                    else stack.offerLast(c);
                 }
             }
             if(stack.isEmpty()) answer++;
         }
 
         System.out.println(answer);
+
+
     }
 }
