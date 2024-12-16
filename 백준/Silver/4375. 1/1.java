2b28ac70c38152cba0f1da94d0305ae341cfe661
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-17 00:11:45 +0900

    [Silver III] Title: 1, Time: 60 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = "";
+        while((s = br.readLine()) != null && !s.isEmpty()){
+            int n = Integer.parseInt(s);
+            int k =0;
+            for (int i=1;;i++) {
+                k = k*10 +1;
+                k = k%n;
+                if (k==0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
