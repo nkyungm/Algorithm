52107394031d04131e886e215c18cff5b38035b3
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-16 17:54:06 +0900

    [Silver IV] Title: 주몽, Time: 124 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -19,18 +19,12 @@ public class Main {
         }
         Arrays.sort(arr);
 
-        // 실수한 부분 : arr[i] 와 arr[M-arr[i]]가 같은 숫자일때 체크를 안해줌..
         for(int i=0;i<N;i++){
             if(M/2 < arr[i]) break;
-            if(!notVisited[arr[i]]) continue;
-            if(!notVisited[M-arr[i]]) continue;
-            if(arr[i] == M-arr[i]) continue;
+            if(!notVisited[arr[i]] || !notVisited[M-arr[i]] || arr[i] == M-arr[i]) continue;
             answer++;
-            notVisited[arr[i]] = false;
-            notVisited[M - arr[i]] = false;
         }
 
         System.out.println(answer);
-
     }
 }
