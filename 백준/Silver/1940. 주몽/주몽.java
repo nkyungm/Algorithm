147250e180e07637a223516b643e286c86d559d9
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-16 18:03:27 +0900

    [Silver IV] Title: 주몽, Time: 124 ms, Memory: 15088 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +32,7 @@ public class Main {
                 end--;
             }
         }
-        
+
         System.out.println(answer);
     }
 }
