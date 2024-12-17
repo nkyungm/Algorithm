418e43011d4cbe592ebfc4fb23ddcdcf27ac6468
남경민 <97418768+nkyungm@users.noreply.github.com>
2024-12-18 01:08:11 +0900

    [Silver III] Title: 빈도 정렬, Time: 196 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -8,24 +8,31 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        Map<Integer,Integer> map = new LinkedHashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> idxMap = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             int key = Integer.parseInt(st.nextToken());
+            // 처음 hashMap에 들어가는 경우 idxMap에 idx 저장
+            if(!map.containsKey(key)) idxMap.put(key,i);
             map.put(key,map.getOrDefault(key,0)+1);
         }
 
         // hashmap value순으로 정렬하기
         List<Integer> keySet = new ArrayList<>(map.keySet());
         keySet.sort((o1,o2) -> {
+            // value가 같으면 idxMap의 value값이 작은 순서대로 정렬
+            if(map.get(o1) == map.get(o2)){
+                return idxMap.get(o1).compareTo(idxMap.get(o2));
+            }
             return map.get(o2).compareTo(map.get(o1));
         });
 
         for(int key : keySet){
-            String k = String.valueOf(key);
-            k += " ";
-            sb.append(k.repeat(map.get(key)));
+            for(int i = 0;i<map.get(key);i++){
+                sb.append(key).append(" ");
+            }
         }
         System.out.println(sb);
     }
