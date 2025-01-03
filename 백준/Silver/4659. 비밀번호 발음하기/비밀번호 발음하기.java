f32c04edcde78103808e73347830bea890faa737
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-01-03 19:51:58 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            String s = br.readLine();
+            if(s.equals("end")) break;
+            char[] arr = s.toCharArray();
+            // set에 모음 넣기
+            Set<Character> set = new HashSet<>();
+            set.add('a');
+            set.add('e');
+            set.add('i');
+            set.add('o');
+            set.add('u');
+            // 모음 확인 변수
+            boolean flag = false;
+            boolean end = false;
+            // 연속 모음, 자음 확인 변수
+            int mo=0, ja = 0;
+
+            for(int i=0;i<arr.length;i++){
+                //1번 조건
+                if(set.contains(arr[i])) flag = true;
+                //2번 조건
+                if(set.contains(arr[i])) {
+                    mo++;
+                    ja = 0;
+                }else{
+                    ja++;
+                    mo=0;
+                }
+                if(mo == 3 || ja == 3){
+                    sb.append("<").append(s).append("> is not acceptable.\n");
+                    end = true;
+                    break;
+                }
+                // 3번 조건
+                if(i==arr.length-1 || arr[i] == 'e' || arr[i] =='o') continue;
+                if(arr[i] != arr[i+1]) continue;
+                sb.append("<").append(s).append("> is not acceptable.\n");
+                end = true;
+                break;
+            }
+            // 1번 종료 조건
+            if(end) continue;
+            if(flag) sb.append("<").append(s).append("> is acceptable.\n");
+            else sb.append("<").append(s).append("> is not acceptable.\n");
+        }
+        System.out.println(sb);
+    }
+}
