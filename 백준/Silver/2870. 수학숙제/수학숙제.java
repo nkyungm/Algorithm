3fe8a04c803c835efdbd90f88ffa13a9d2be4c75
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-01-03 22:13:14 +0900

    [Silver IV] Title: 수학숙제, Time: 184 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,63 +1,51 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+    static StringBuilder sbb = new StringBuilder();
+    static ArrayList<String> answer = new ArrayList<>();
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb =new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        List<String> answer = new ArrayList<>();
 
         while(N --> 0){
-            char[] arr = br.readLine().toCharArray();
-            for(int i=0;i<arr.length;i++){
-                // 소문자인 경우
-                if(arr[i] >= 'a' && arr[i] <= 'z'){
-                    // sb에 숫자 있으면 배열에 넣기
-                    String num = sb.toString();
-                    if(num.isEmpty()) continue;
-                    char[] c = num.toCharArray();
-                    // 1. size가 1인경우 그냥 넣기
-                    // if(c.length == 1) answer.add(num);
-                    // 2. 0빼고 맨 앞의 인덱스 찾기
-                    // 중요 : 다 0인 경우 고려
-                    int idx = 0;
-                    for(int j=0;j<c.length;j++){
-                        if(c[j] == '0') continue;
-                        idx = j;
-                        break;
-                    }
-                    if(c[idx] == '0') answer.add("0");
-                    else answer.add(num.substring(idx,c.length));
-
-                    sb = new StringBuilder();
-                }else{
-                    sb.append(arr[i]);
+            char[] cArr = br.readLine().toCharArray();
+            for(char c : cArr){
+                // 소문자일 경우 go()
+                if(c >='a' && c<='z'){
+                    if(sbb.length() != 0) go();
+                }
+                // 숫자일 경우 sbb에 넣기
+                else{
+                    sbb.append(c);
                 }
             }
-            String num = sb.toString();
-            if(!num.isEmpty()) {
-                char[] c = num.toCharArray();
-                int idx = 0;
-                for(int j=0;j<c.length;j++){
-                    if(c[j] == '0') continue;
-                    idx = j;
-                    break;
-                }
-                if(c[idx] == '0') answer.add("0");
-                else answer.add(num.substring(idx,c.length));
-            }
-            sb = new StringBuilder();
+            if(sbb.length() != 0) go();
         }
         // 정렬
-        Collections.sort(answer,(a1,a2) ->{
+        answer.sort((a1,a2) ->{
             if(a1.length() == a2.length()) return a1.compareTo(a2);
             else return a1.length() - a2.length();
         });
 
         // 출력
-        for(int i=0;i<answer.size();i++) sb.append(answer.get(i)).append("\n");
+        for (String s : answer) sb.append(s).append("\n");
 
         System.out.println(sb);
+    }
+    // 숫자 배열에 넣는 함수
+    static void go() {
+        // 1. while문 돌리면서 맨 앞이 0이고 길이가 0보다 클때 맨 앞에 지우기
+        while(true){
+            if(sbb.length() != 0 && sbb.charAt(0) == '0'){
+                sbb.delete(0,1);
+            }
+            else break;
+        }
+        // 길이가 0인경우 "0" 넣기
+        if(sbb.length()==0) sbb.append("0");
+        answer.add(sbb.toString());
+        sbb =new StringBuilder();
     }
 }
