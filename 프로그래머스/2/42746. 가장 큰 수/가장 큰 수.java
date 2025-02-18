d54a40f7f306cf50d54d8cebdd212ec859207e95
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-19 00:32:39 +0900

    [level 2] Title: 가장 큰 수, Time: 176.97 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,23 @@
 import java.util.*;
-import java.lang.*;
-/**
-- 자리별로 큰수 넣기
-*/
+import java.io.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        int L = numbers.length;
-        String[] nStr = new String[L];
-        
-        for(int i=0;i<L;i++){
-            nStr[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(nStr,(o1,o2)->(o2+o1).compareTo(o1+o2));
-        
-        if(nStr[0].equals("0")) return "0";
-        
         StringBuilder sb = new StringBuilder();
+        // 1. 숫자말고 String으로 변환
+        // 2. o1+o2랑 o2+o1중 더 큰거 비교 => 붙였을때를 비교하는 거니까 610,106 비교
+        String[] arr = new String[numbers.length];
         
-        for(int i=0;i<L;i++){
-            sb.append(nStr[i]);
+        for(int i=0;i<numbers.length;i++){
+            arr[i] = String.valueOf(numbers[i]);
         }
         
+        Arrays.sort(arr,(o1,o2)-> (o2+o1).compareTo(o1+o2));
+        
+        // 중요) 처음이 0인 경우는 무조건 0 출력
+        if(arr[0].equals("0")) return "0";
+        
+        for(int i=0;i<numbers.length;i++) sb.append(arr[i]);
         answer = sb.toString();
         
         return answer;
