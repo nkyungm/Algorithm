050012c7c9f76f60049f587af4da9790f2759c9f
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-18 23:44:04 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,11 +1,9 @@
 -- 코드를 작성해주세요
 # 400 = 110010000 && 해서 256나오면
 # 256 = 100000000
-select id,email,first_name,last_name
-from DEVELOPERS
-where
-(skill_code & (select code from SKILLCODES where name in ('Python')))
-= (select code from SKILLCODES where name in ('Python')) or
-(skill_code & (select code from SKILLCODES where name in ('C#')))
-= (select code from SKILLCODES where name in ('C#'))
+select distinct(id),email,first_name,last_name
+from DEVELOPERS d
+join skillcodes s
+on (d.skill_code & s.code) = s.code
+where s.name in ('Python','C#')
 order by id;
