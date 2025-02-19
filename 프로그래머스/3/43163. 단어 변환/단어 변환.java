e181cb224ed655c8dc9a686910cdf01dd4311252
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-19 18:48:53 +0900

    [level 3] Title: 단어 변환, Time: 0.46 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,58 +1,55 @@
 import java.util.*;
-
-// 알고리즘 : BFS(도달하는 최소값 찾기)
-/*
-- visited 방문처리 필요
-- queue에 배열값 넣기([0]: string, [1]: int)
-1. 처음 words에서 target값이 있는 지 확인하고 없으면 return 0
-2. begin에서 words for문 돌아가면서 방문X and 알파벳 한개만 다르면 queue에 넣기
-3. queue에서 빼면서 target값과 동일하면 return
-*/
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        answer = bfs(begin,target,words);
+        int answer = BFS(begin,target,words);
+        // 최단 변화수 : BFS
+        // 1. queue에 넣고 words 처음에 돌리면서 1개만 다른 경우 큐에 넣고 방문처리
+        // 2. queue값중에 cog가 있으면 종료
+        // 3. queue는 int[] 저장
+        
         return answer;
     }
-    static int bfs(String begin, String target,String[] words){
-        Queue<Word> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length]; // 방문처리 함수
+    static int BFS(String begin,String target,String[] words){
+        Queue<Word> queue= new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
         
         queue.add(new Word(begin,0));
+        
         while(!queue.isEmpty()){
-            Word w = queue.poll();
-            if(w.s.equals(target)) {
-                return w.cnt;
-            }
+            Word v = queue.poll();
+            
+            // 종료조건
+            if(v.s.equals(target)) return v.num;
             
             for(int i=0;i<words.length;i++){
                 if(visited[i]) continue;
-                // 해당 단어와 알파벳 한개만 다른지 확인
-                if(!diffCnt(w.s,words[i])) continue;
+                // 한개만 다른지 확인
+                if(!diff(v.s,words[i])) continue;
                 visited[i] = true;
-                queue.add(new Word(words[i],w.cnt+1));
+                queue.add(new Word(words[i],v.num+1));
             }
         }
-        
         return 0;
     }
-    // 알파벳별 다른 개수 확인
-    static boolean diffCnt(String target,String word){
-        int cnt = 0;
-        for(int i=0;i<target.length();i++){
-            if(target.charAt(i)!=word.charAt(i)) cnt++;
-            if(cnt > 1) return false;
+    static boolean diff(String s1,String s2){
+        if(s1.length() != s2.length()) return false;
+        int n = 0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i) == s2.charAt(i)) continue;
+            else {
+                n++;
+                if(n >=2) return false;
+            }
         }
-        if(cnt ==0) return false;
         return true;
     }
     static class Word{
         String s;
-        int cnt;
-        public Word(String s,int cnt){
+        int num;
+        
+        public Word(String s,int num){
             this.s = s;
-            this.cnt = cnt;
+            this.num = num;
         }
     }
 }
