4dfe1e3a2f6317762b9565d676d1ec5fdc8d3c4e
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-19 18:30:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.99 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,40 @@
 import java.util.*;
-
-/**
-BFS 최단거리
-*/
-
 class Solution {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static Queue<Robot> queue = new ArrayDeque<>();
-    static boolean[][] visited;
-    static int M,N;
     public int solution(int[][] maps) {
-        int answer = 0;
-        M = maps[0].length;
-        N = maps.length;
-        visited = new boolean[N][M];
-        queue.add(new Robot(0,0,1));
-        visited[0][0] = true;
-        answer = bfs(maps);
+        int answer = BFS(maps);
+        // 최소 거리 : BFS
+        
         return answer;
     }
-    static int bfs(int[][] maps){
+    
+    static int[] dx = new int[]{0,0,1,-1};
+    static int[] dy = new int[]{1,-1,0,0};
+    static int BFS(int[][] maps){
+        int n = maps.length;
+        int m = maps[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][m];
+        
+        queue.add(new int[]{0,0,1});
+        visited[0][0] = true;
+        
         while(!queue.isEmpty()){
-            Robot r = queue.poll();
-            if(r.x == N-1 && r.y == M-1) return r.cnt;
+            int[] v = queue.poll();
+            
+            // 종료 조건
+            if(v[0] == n-1 && v[1] == m-1) return v[2];
             
             for(int i=0;i<4;i++){
-                int xi = r.x + dx[i];
-                int yi = r.y + dy[i];
-                if(xi <0 || xi>=N || yi<0 || yi>=M) continue;
-                if(maps[xi][yi]==0 || visited[xi][yi]) continue;
-                
-                queue.add(new Robot(xi,yi,r.cnt+1));
+                int xi = dx[i] + v[0];
+                int yi = dy[i] + v[1];
+                if(xi <0 || xi>=n || yi <0 || yi>=m) continue;
+                if(visited[xi][yi]) continue;
+                if(maps[xi][yi] ==0) continue;
                 visited[xi][yi] = true;
+                queue.add(new int[]{xi,yi,v[2]+1});
             }
         }
+        
         return -1;
-    }
-    
-    static class Robot{
-        int x;
-        int y;
-        int cnt;
-        Robot(int x,int y,int cnt){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
     }
 }
