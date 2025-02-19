729e5c675062d4dd4360a2a8071d21ee6346f501
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-19 17:53:27 +0900

    [level 2] Title: 타겟 넘버, Time: 4.33 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,34 +1,21 @@
 import java.util.*;
 import java.io.*;
 class Solution {
+    static int num=0;
     public int solution(int[] numbers, int target) {
-        int answer = BFS(numbers,target);
+        DFS(-1,0,numbers,target);
         // 해당 개수 구하기 : DFS, BFS
-        // visited 방문배열을 만들어서 큐에 없으면 종료
-        // 큐넣을때) 방문 체크하고 false면, 다음인덱스 값 +,- 큐에 넣기
-        // 그냥 큐넣을때 마지막 노드일 경우, 개수 계산
-        
+        int answer = num;
         return answer;
     }
-    static int BFS(int[] numbers,int target){
-        int num=0;
-        // 큐 생성
-        Queue<int[]> queue = new ArrayDeque<>();
-        // boolean[] visited = new boolean[numbers.length];
-        // 첫번째 요소 넣기
-        queue.add(new int[]{0,numbers[0]});
-        queue.add(new int[]{0,-numbers[0]});
-        // visited[0] = true;
-        while(!queue.isEmpty()){
-            int[] v = queue.poll();
-            // 확인 조건
-            if(v[0]==numbers.length-1) {
-                if(v[1]==target) num++;
-                continue;
-            }
-            queue.add(new int[]{v[0]+1,v[1]+numbers[v[0]+1]});
-            queue.add(new int[]{v[0]+1,v[1]-numbers[v[0]+1]});
+    static void DFS(int idx,int total,int[] numbers,int target){
+        // 종료 조건
+        if(idx == numbers.length-1){
+            if(total == target) num++;
+            return;
         }
-        return num;
+        // 반복 (그 뒤의 인덱스 +,- 더해 재귀돌림)
+        DFS(idx+1,total+numbers[idx+1],numbers,target);
+        DFS(idx+1,total-numbers[idx+1],numbers,target);
     }
 }
