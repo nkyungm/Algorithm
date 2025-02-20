aa47ec5b0eea2ddb33cc600ff409c244154ba465
남경민 <97418768+nkyungm@users.noreply.github.com>
2025-02-20 14:50:12 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
-SELECT ao.animal_id, ao.name from animal_outs ao
-join animal_ins ai
-on ao.animal_id = ai.animal_id
+select ai.animal_id,ai.name
+from animal_ins ai
+join animal_outs ao
+on ai.animal_id = ao.animal_id
 where ai.datetime > ao.datetime
 order by ai.datetime;
