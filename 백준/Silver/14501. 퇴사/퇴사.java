f5b945c1ab8e9cdab19090f226978e381d0e6948
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-04-08 00:03:28 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,55 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
     static int N;
     static int[][] arr;
-    static List<Integer> list;
-
-    static int ans = 0;
-    public static void main(String[] args) throws IOException {
+    static int score=0;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =null;
+        StringTokenizer st = null;
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][2];
-        list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
+        arr = new int[N+2][2];
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        setCall(0);
-        System.out.println(ans);
-    }
-    private static void setCall(int day){
-        // 기저 조건
-        if(day >= N){
-            calculateMaxValue(list);
-            return;
+            arr[i+1][0] = Integer.parseInt(st.nextToken()); // T
+            arr[i+1][1] = Integer.parseInt(st.nextToken()); // P
         }
 
-        for (int i = day; i < N; i++) {
-            if(i+arr[i][0] <= N) {
-                list.add(i);
-                setCall(i+arr[i][0]);
-                list.remove(list.indexOf(i));
+        arr[N+1][0] = 5;
+        arr[N+1][1] = 0;
+
+        BFS();
+        System.out.println(score);
+
+    }
+    private static void BFS(){
+        Deque<Job> queue = new ArrayDeque<>();
+        queue.add(new Job(1,false,0));
+        if(N+1 >= 1+arr[1][0]) queue.add(new Job(1,true,arr[1][1]));
+
+        while(!queue.isEmpty()) {
+            Job j = queue.poll();
+
+            // max 체크 && N 체크
+            score = Math.max(score, j.score);
+            if(j.idx == N) continue;
+
+            int next = j.idx;
+            // false인 경우
+            if (!j.visited && j.idx + 1 <= N + 1) {
+                next = j.idx + 1;
+                // idx+1 true
+                if (arr[next][0] + next <= N + 1) {
+                    queue.add(new Job(next, true, j.score + arr[next][1]));
+                }
+                // idx+1 false
+                queue.add(new Job(next, false, j.score));
             }
-            setCall(i+arr[i][0]);
+
+            // true인 경우
+            if (j.visited && j.idx + arr[j.idx][0] <= N + 1) {
+                next = j.idx + arr[j.idx][0];
+                // next true
+                if (arr[next][0] + next <= N + 1) {
+                    queue.add(new Job(next, true, j.score + arr[next][1]));
+                }
+                // next false
+                queue.add(new Job(next, false, j.score));
+            }
         }
+
     }
 
-    private static void calculateMaxValue(List<Integer> list){
-        int total = 0;
-        for (int i = 0; i < list.size(); i++) {
-            total+= arr[list.get(i)][1];
+    static class Job{
+        int idx;
+        boolean visited;
+        int score;
+
+        public Job(int idx,boolean visited,int score){
+            this.idx = idx;
+            this.visited = visited;
+            this.score = score;
         }
-        ans = Math.max(ans,total);
     }
 }
