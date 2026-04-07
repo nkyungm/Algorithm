7bc419952b9e8ddcecf79644d5370307b34b1a0e
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-04-08 00:48:50 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 public class Main {
     static int N;
     static int[][] arr;
@@ -9,67 +13,30 @@ public class Main {
         StringTokenizer st = null;
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N+2][2];
+        arr = new int[N+1][2];
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             arr[i+1][0] = Integer.parseInt(st.nextToken()); // T
             arr[i+1][1] = Integer.parseInt(st.nextToken()); // P
         }
 
-        arr[N+1][0] = 5;
-        arr[N+1][1] = 0;
-
-        BFS();
+        DFS(1,0);
         System.out.println(score);
 
     }
-    private static void BFS(){
-        Deque<Job> queue = new ArrayDeque<>();
-        queue.add(new Job(1,false,0));
-        if(N+1 >= 1+arr[1][0]) queue.add(new Job(1,true,arr[1][1]));
-
-        while(!queue.isEmpty()) {
-            Job j = queue.poll();
-
-            // max 체크 && N 체크
-            score = Math.max(score, j.score);
-            if(j.idx == N) continue;
-
-            int next = j.idx;
-            // false인 경우
-            if (!j.visited && j.idx + 1 <= N + 1) {
-                next = j.idx + 1;
-                // idx+1 true
-                if (arr[next][0] + next <= N + 1) {
-                    queue.add(new Job(next, true, j.score + arr[next][1]));
-                }
-                // idx+1 false
-                queue.add(new Job(next, false, j.score));
-            }
-
-            // true인 경우
-            if (j.visited && j.idx + arr[j.idx][0] <= N + 1) {
-                next = j.idx + arr[j.idx][0];
-                // next true
-                if (arr[next][0] + next <= N + 1) {
-                    queue.add(new Job(next, true, j.score + arr[next][1]));
-                }
-                // next false
-                queue.add(new Job(next, false, j.score));
-            }
+    private static void DFS(int day, int profit){
+        // 종료조건
+        if(day >= N+1){
+            score = Math.max(score,profit);
+            return;
         }
 
-    }
-
-    static class Job{
-        int idx;
-        boolean visited;
-        int score;
-
-        public Job(int idx,boolean visited,int score){
-            this.idx = idx;
-            this.visited = visited;
-            this.score = score;
+        //상담 하는 경우
+        if(day + arr[day][0] <= N+1){
+            DFS(day+ arr[day][0],profit+arr[day][1]);
         }
+        //상담 안하는 경우
+        DFS(day+1,profit);
     }
+
 }
