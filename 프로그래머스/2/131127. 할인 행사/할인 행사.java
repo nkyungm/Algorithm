5701065c3297dd420c32243ae028af1c332557e0
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-04-19 18:36:23 +0900

    [level 2] Title: 할인 행사, Time: 60.55 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 class Solution {
+    static HashMap<String,Integer> wantMap;
     static HashMap<String,Integer> discountMap;
     // 알고리즘 : HaspMap + 슬라이딩 윈도우
     public int solution(String[] want, int[] number, String[] discount) {
@@ -8,35 +9,70 @@ class Solution {
         
         int startIdx = 0;
         int endIdx = 0;
+        int matchCount = 0;
+        wantMap = new HashMap<>();
         discountMap = new HashMap<>();
+        
+        // wantMap 세팅
+        for(int i=0;i<want.length;i++){
+            wantMap.put(want[i],number[i]);
+        }
         
         // discount 처음 세팅 (0~9)
         for(int i=0;i<10;i++){
             discountMap.put(discount[i],discountMap.getOrDefault(discount[i],0)+1);
         }
         endIdx = 9;
-        if(countCheck(want,number)) answer++;
+        
+        // matchCount 계산
+        for(String key : wantMap.keySet()){
+            if(discountMap.getOrDefault(key,0) >= wantMap.get(key)) matchCount++;
+        }
+        
+        // 현재 처음 range에서 충족하는지 확인
+        if(matchCount == wantMap.size()) answer++;
         
         // 종료조건 : endIdx가 discount 마지막에 도달했을때
         while(endIdx < discount.length-1){
-            // startIdx 전에꺼 빼고 +1
-            discountMap.put(discount[startIdx],discountMap.get(discount[startIdx])-1);
+            // startIdx 확인 + matchCount 체크
+            String removeItem = discount[startIdx];
+            boolean matchFlag = false;
+            
+            // 이전 match 확인
+            if(wantMap.containsKey(removeItem) && discountMap.get(removeItem) >= wantMap.get(removeItem)) matchFlag = true;
+            
+            // wantMap에 있는지 확인 + matchCount 감소 확인
+            discountMap.put(removeItem,discountMap.get(removeItem)-1);
+            if(wantMap.containsKey(removeItem)){
+                if(matchFlag && discountMap.get(removeItem) < wantMap.get(removeItem)) matchCount--;
+            } 
             startIdx ++;
-            // endIdx도 동일하게 진행 (반대로)
+            
+            // endIdx 확인
             endIdx ++;
-            discountMap.put(discount[endIdx],discountMap.getOrDefault(discount[endIdx],0)+1);
-            // wantMap와 discountMap 개수 포함되는지 체크
-            if(countCheck(want,number)) answer++;
+            matchFlag = false;
+            String addItem = discount[endIdx];
+            
+            // 추가 match 확인
+            if(wantMap.containsKey(addItem) && discountMap.getOrDefault(addItem,0) < wantMap.get(addItem)) matchFlag = true;
+            
+            discountMap.put(addItem,discountMap.getOrDefault(addItem,0)+1);
+            // wantMap에 있는지 확인 + matchCount 증가 확인
+            if(wantMap.containsKey(addItem)){
+                if(matchFlag && discountMap.get(addItem) >= wantMap.get(addItem)) matchCount++;
+            }
+            
+            if(matchCount == wantMap.size()) answer++;
         }
         
         return answer;
     }
-    private static boolean countCheck(String[] want, int[] number){
-        for(int i=0;i<want.length;i++){
-            // discount에 개수 충족되는지 체크
-            if(!discountMap.containsKey(want[i])) return false;
-            if(discountMap.get(want[i]) < number[i]) return false;
-        }
-        return true;
-    }
+    // private static boolean countCheck(String[] want, int[] number){
+    //     for(int i=0;i<want.length;i++){
+    //         // discount에 개수 충족되는지 체크
+    //         if(!discountMap.containsKey(want[i])) return false;
+    //         if(discountMap.get(want[i]) < number[i]) return false;
+    //     }
+    //     return true;
+    // }
 }
