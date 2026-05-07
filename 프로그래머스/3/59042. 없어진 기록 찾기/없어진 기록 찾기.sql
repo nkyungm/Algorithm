9daec4f05e2b12bebb4ca09b2e5c3c5c7bbbf878
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-05-07 23:23:40 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,5 @@
 -- 코드를 입력하세요
-select ao.animal_id,ao.name
-from animal_outs ao
-left join animal_ins ai
+SELECT ao.animal_id,ao.name from ANIMAL_OUTS ao
+left join ANIMAL_INS ai
 on ao.animal_id = ai.animal_id
-where ai.animal_id is null
-order by ao.animal_id;
+where ai.animal_id is null;
