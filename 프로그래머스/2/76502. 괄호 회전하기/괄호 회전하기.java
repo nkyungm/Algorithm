f8a1b6644d7e5f89255a3837034d33483ef0f158
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-05-07 23:12:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 14.01 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,36 +4,47 @@ import java.io.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        char temp = '1';
         
-        // 왼쪽으로 회전하면서 알맞은 문자열인지 확인
-        answer += go(s);
-        for(int i=0;i<s.length()-1;i++){
-            String first = s.substring(0,1);
-            s = s.substring(1) + first;
-            // System.out.println(s);
-            answer+= go(s);
+        // char 배열로 변경
+        char[] arr = s.toCharArray();
+        // 2) stack으로 올바른 문자열인지 판단 (함수)
+        if(check(arr)) answer++;
+            
+        // 1) 회전
+        
+        for(int i=0;i<s.length()-1;i++){ // s-1만큼 회전
+            temp = arr[0];
+            for(int j=1;j<arr.length;j++){
+                arr[j-1] = arr[j];
+            }
+            arr[arr.length-1] = temp;
+            // 2) stack으로 올바른 문자열인지 판단 (함수)
+            if(check(arr)) answer++;
         }
         
         
         return answer;
     }
-    // 올바른 괄호 문자열인지 확인 함수
-    private static int go(String s){
+    private static boolean check(char[] arr){
         Deque<Character> stack = new ArrayDeque<>();
+        // stack 삽입 : push , 삭제 pop
         
-        for(char c :s.toCharArray()){
-            if(c =='[' || c== '(' || c=='{'){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()) return 0;
+        for(char c : arr){
+            if(c=='[' || c=='(' || c=='{' ) stack.push(c);
+            else{
+                // stack 비었으면 return
+                if(stack.isEmpty()) return false;
+                // 짝 안맞으면 return
                 char cp = stack.pop();
-                if(c==']' && cp!='[') return 0;
-                if(c==')' && cp!='(') return 0;
-                if(c=='}' && cp!='{') return 0;
+                if(c==']' && cp!='[') return false;
+                if(c==')' && cp!='(') return false;
+                if(c=='}' && cp!='{') return false;
             }
         }
         
-        if(!stack.isEmpty()) return 0;
-        return 1;
+        // stack 안비었으면 false
+        if(!stack.isEmpty()) return false;
+        return true;
     }
 }
