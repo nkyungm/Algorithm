4e6362fca8f259507fcc3c5796954f51fd5d16d9
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-05-08 22:01:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.18 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,28 +1,27 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
+        // hashmap
         HashMap<String,Integer> map = new HashMap<>();
-        
-        for(int i=0;i<participant.length;i++){
-            String name = participant[i];
-            map.put(name,map.getOrDefault(name,0)+1);
+        for(String p : completion){
+            map.put(p,map.getOrDefault(p,0)+1);
         }
         
-        // completion에서 빼기
-        for(int i=0;i<completion.length;i++){
-            String name = completion[i];
-            if(!map.containsKey(name)) continue;
-            
-            if(map.get(name) == 1) map.remove(name);
-            else if(map.get(name) > 1){
-                map.put(name,map.get(name)-1);
+        for(String person : participant){
+            // 있으면 -1하고 넘기기
+            if(!map.containsKey(person)){
+                answer = person;
+            }else{
+                if(map.get(person) == 1){
+                    // key 삭제
+                    map.remove(person);
+                }else{
+                    map.put(person,map.get(person)-1);
+                }
             }
-            
         }
-        
-        answer = map.keySet().iterator().next();
         
         return answer;
     }
