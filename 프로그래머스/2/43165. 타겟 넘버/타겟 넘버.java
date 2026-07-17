53ab80aa907c0dc08ab9eb99f36e8f968885d970
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-07-17 21:10:49 +0900

    [level 2] Title: 타겟 넘버, Time: 3.18 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 class Solution {
-    static int ans=0;
+    int ans=0;
     public int solution(int[] numbers, int target) {
         
         // 그래프 완탐 (DFS)
         // DFS 함수 안에서 +,-로 둘다 넣기
         // 종료조건(순서가 n일때 , target과 같으면 ans++)
         int n = numbers.length;
-        DFS(numbers,n,target,-1,0);
+        DFS(numbers,n,target,0,0);
         
         return ans;
     }
     void DFS(int[] numbers, int n,int target, int cur,int sum){
         // 종료조건(순서가 n일때 , target과 같으면 ans++)
-        if(cur == n-1){
+        if(cur == n){
             if(target == sum) ans++;
             return;
         }
         
         // 재귀 조건
-        DFS(numbers,n,target,cur+1,sum+numbers[cur+1]);
-        DFS(numbers,n,target,cur+1,sum-numbers[cur+1]);
+        DFS(numbers,n,target,cur+1,sum+numbers[cur]);
+        DFS(numbers,n,target,cur+1,sum-numbers[cur]);
     }
 }
