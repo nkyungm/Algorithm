9107c64d3ea6d7e0ffcd6b1afd4efd8eb3c7d54a
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-07-21 00:30:21 +0900

    [level 2] Title: 오픈채팅방, Time: 54.42 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,47 +1,50 @@
 import java.util.*;
 import java.io.*;
-
 class Solution {
     public String[] solution(String[] record) {
-        List<String[]> list = new ArrayList<>();
-        
-        // 1. hashmap (key = id,value = 닉네임) 생성
-        // 2. enter -> hashmap에 없으면 추가, answer에도 추가
-        // => 있는데 닉네임이 다르면 변경하고 answer 추가
-        // 3. leave -> answer에만 추가
-        // 4. change -> hashmap 값변경
+        String[] answer = {};
+        List<String> arr = new ArrayList<>();
+        StringTokenizer st = null;
         
         HashMap<String,String> map = new HashMap<>();
         
+        // Hash Map에 {id, nickname} 저장
+        int n =0;
         for(int i=0;i<record.length;i++){
-            StringTokenizer st = new StringTokenizer(record[i]);
-            String command = st.nextToken();
+            st = new StringTokenizer(record[i]);
+            String comm = st.nextToken();
             String id = st.nextToken();
-            String name;
-            switch(command){
-                case "Enter":
-                    name = st.nextToken();
-                    if(map.get(id)== null) map.put(id,name);
-                    else map.put(id,name);
-                    list.add(new String[]{id,"Enter"});
-                    break;
-                case "Leave":
-                    list.add(new String[]{id,"Leave"});
-                    break;
-                case "Change":
-                    name = st.nextToken();
-                    map.put(id,name);
-                    break;
+            String nickname = "";
+            if(!comm.equals("Leave")){
+                nickname = st.nextToken();
+                map.put(id,nickname);
+            }
+            
+            if(!comm.equals("Change")){
+                n++;
             }
         }
-        String[] answer = new String[list.size()];
-        for(int i=0;i<list.size();i++) {
-            String[] s = list.get(i);
-            if(s[1].equals("Enter")) answer[i] = map.get(s[0])+"님이 들어왔습니다.";
-            else answer[i] = map.get(s[0])+"님이 나갔습니다.";
+        
+        answer = new String[n];
+        int j=0;
+        for(int i=0;i<record.length;i++){
+            st = new StringTokenizer(record[i]);
+            String comm = st.nextToken();
+            String id = st.nextToken();
+            String nickname = "";
+            if(!comm.equals("Leave")){
+                nickname = st.nextToken();
+            }
+            
+            if(comm.equals("Change")) continue;
+            if(comm.equals("Enter")){
+                answer[j] = map.get(id)+"님이 들어왔습니다.";
+            }else{
+                answer[j] = map.get(id)+"님이 나갔습니다.";
+            }
+            j++;
         }
-        
-        
+
         return answer;
     }
 }
