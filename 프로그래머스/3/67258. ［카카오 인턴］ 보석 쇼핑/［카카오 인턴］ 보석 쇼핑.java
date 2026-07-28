47a5987bbf0083c9407c06b98354a18883f0b66d
남경민 <97418768+nkyungm@users.noreply.github.com>
2026-07-29 02:12:31 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 30.55 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,64 +1,51 @@
-
 import java.util.*;
-
-// 알고리즘 : 투포인터
-/*
-- 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간 찾기
-1. Set으로 개수 세기 = N
-2. 시작점, 끝점 0 투포인터 시작
-3. 포인터의 흐름을 담는 Hashmap 선언
-4. right 포인터 N과 map 크기 같아질때까지 이동
-5. left 포인터 이동(N > map크기 일때까지 이동)
-- 이동할때 해당개수 -1하면서 이동
-- map size == N 인 경우, distance 최솟값 저장하면서 이동
-6. (4,5)과정을 배열끝에 도달할때까지 반복
-*/
-
 class Solution {
     public int[] solution(String[] gems) {
         int[] answer = {};
-        int N = 0;
-        int gems_size = gems.length;
-        
-        // 1. HashSet으로 개수 세기 = N
-        Set<String> set = new HashSet<String>(Arrays.asList(gems));
+        int ansStart = 0;
+        int ansEnd = gems.length;
+        // 전체 gems 저장 hashMap
+        // 1. 총 보석의 개수 저장
+        // 2. 뒤에서 빼다가 더이상 못빼면 앞에서 빼기
+        int totalCnt = 0;
+        HashMap<String,Integer> gemsMap = new HashMap<>();
         HashMap<String,Integer> map = new HashMap<>();
-        
-        N = set.size();
-        
-        // 2. 투포인터 첫번째 인덱스, 마지막 인덱스 0 에서 시작하기
-        int left =0;
-        int right = 0;
-        int distance = Integer.MAX_VALUE;
-        int[] distanceArr = {};
-
-        // 투포인터 시작
-        while(true){
-            if (map.size() == N){
-                // left 포인터 이동(N > map크기 일때까지 이동)
-                map.put(gems[left],map.get(gems[left])-1);
-                if(map.get(gems[left])==0) map.remove(gems[left]);
-                left++;
-            }else if(right == gems_size){
-                break;
-            }
-            else {
-                // right 포인터 N과 map 크기 같아질때까지 이동
-                map.put(gems[right],map.getOrDefault(gems[right],0)+1);
-                right++;
-            }
-            
-            // map size == N 인 경우, distance 최솟값 저장하면서 이동
-            if(map.size() == N){
-                if(distance > (right-left+1)) {
-                distance = (right-left+1);
-                distanceArr = new int[]{left+1,right};
-                }
-            }
-            
+        // 저장
+        for(String gem : gems){
+            gemsMap.put(gem, gemsMap.getOrDefault(gem,0)+1);
         }
-        answer = distanceArr;
-        return answer;
+        // 보석 전체 종류
+        totalCnt = gemsMap.size();
         
+        // 둘다 왼쪽에서 시작
+        int start =0;
+        int end = 0;
+        
+        // 투포인터
+        while(true){
+            // map이 모든 종류가 포함되는지 확인 -> 아니면 end++
+            if(map.size() < totalCnt){
+                if(end == gems.length) break;
+                map.put(gems[end],map.getOrDefault(gems[end],0)+1);
+                end++;
+            }else{ // 모든 종류인 경우
+                // 현재 구간(start ~ end-1)이 정답 후보, 정답갱신
+                if(ansEnd - ansStart > end -start){
+                    ansStart = start;
+                    ansEnd = end;
+                }
+                // 왼쪽 제거
+                if(map.get(gems[start]) ==1){
+                    map.remove(gems[start]);
+                }else{
+                    map.put(gems[start],map.get(gems[start])-1);
+                }
+                start++;
+            }
+        }
+        
+        answer = new int[]{ansStart+1,ansEnd};
+        
+        return answer;
     }
 }
